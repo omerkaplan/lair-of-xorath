33ab84f9d5f24eecab5062bebff22ebe878bada4
Omer Kaplan <omerkaplan@Omers-MacBook-Pro.local>
2019-03-22 12:21:36 -0400

    kobolds added

--- includes/monsters.py
+++ includes/monsters.py
@@ -17,7 +17,8 @@ class monster:
 # list of monsters and thier stats
 
 wererat = monster('Wererat',12,10,10,random.choice(equipment.t1_weapons),equipment.natural_hide,'chops your head and feasts on your corpse.')
+kobold = monster('Kobold',12,10,10,random.choice(equipment.t1_weapons),equipment.natural_hide,'drags your corpse to his camp where this tribe will feast upon it for days to come.')
 giant_spider = monster('Giant Spider',9,10,10,equipment.bite_attack,equipment.natural_hide,'bites your head off.')
 snakefolk = monster('Snakefolk',12,11,11,equipment.poisoned_spear,equipment.natural_hide,'quickly wraps around you and start crushing. You feel your bones buckle under the pressure.')
 
-monsters = [wererat,giant_spider,snakefolk]
+monsters = [wererat,giant_spider,snakefolk,kobold]
